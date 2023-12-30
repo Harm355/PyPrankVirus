562c0139b994dac507ccbb218b663f2c4dd8d67d
Harm355 <151342662+Harm355@users.noreply.github.com>
2023-12-30 19:32:22 +0545

    main.py
    Create main.py

---
+++ Mas.py
@@ -0,0 +1,29 @@
+result = [1, 2, 3, 4, 5]
+
+def Copydiet(microsoft_name):
+    damage = True
+   
+for socket in result:
+    print(result)
+    Copydiet("Microsoft Agent")
+
+def getr63roblox(name):
+    names = "lucky guy"
+    with open("name.people", "w") as sex:
+        sex.write(names)
+
+    with open("name.people", "rb") as m:
+        diet = m.readline()
+    
+getr63roblox("lucky guy")
+for names in __name__:        
+    print("you got r63 roblox on lucky guy")
+    continue
+
+def getfile(filename):
+    if "files_appended" in filename:
+        exec("cmd /c echo hacked &echo hacked")
+        with open(filename, "r") as di:
+            content = di.read()
+
+getfile("Mas.py")
